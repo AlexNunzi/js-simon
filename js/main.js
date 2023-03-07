d7f2b9c8caf607bc400483c977fec4b282867bb8
Alex Nunzi <alex.nunzi@hotmail.it>
2023-03-07 22:40:56 +0100

    Aggiunta logica di controllo se i numeri inseriti dall'utente sono compresi tra quelli generati randomicamente. Stampa del punteggio a schermo e cambiamento del colore di sfondo degli elementi contenenti i numeri indovinati dall'utente.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const numbersContainerDom = document.querySelector('.numbersContainer');
+const scoreDom = document.getElementById('score');
 const startDom = document.getElementById('start');
 const userMessageDom = document.getElementById('userMessage');
 
@@ -7,25 +8,27 @@ startDom.addEventListener('click', function(){
 
     userMessageDom.classList.add('d-none');
     numbersContainerDom.classList.remove('d-none');
-    let sortedNumber = [];
-    let userNumbers = [];
-    sortedNumber = createSlotsOfRandomNumbers(5, numbersContainerDom, sortedNumber);
+    scoreDom.classList.add('d-none');
+    sortedNumber = createSlotsOfRandomNumbers(5, numbersContainerDom);
     
     console.log(sortedNumber);
     
-    const playDelay = setTimeout(startGame, 3000); //30000
-    const promptDelay = setTimeout(userPrompt, 4000); //31000
+    setTimeout(startGame, 30000); //30000
+    setTimeout(function(){
+        userPrompt(sortedNumber);
+    }, 31000); //31000
 });
 
 
 
 
 
-function createSlotsOfRandomNumbers(quantity, domContainer, blacklistArray){
+function createSlotsOfRandomNumbers(quantity, domContainer){
+    console.log();
     domContainer.innerHTML = '';
     let numbers = [];
     for(i=0; i < quantity; i++){
-        numbers.push(createUniqueNumberedSlot(domContainer, blacklistArray));
+        numbers.push(createUniqueNumberedSlot(domContainer, numbers));
     }
     return numbers;
 }
@@ -36,10 +39,10 @@ function createUniqueNumberedSlot(domContainer, blacklistArray){
     let number = createUniqueRandomNumber(blacklistArray, 0, 100);
     slotsNumber.append(number);
     slotsNumber.classList.add('number');
+    slotsNumber.classList.add('d-flex');
     newSlot.append(slotsNumber);
     newSlot.classList.add('slot');
     domContainer.append(newSlot);
-    console.log(number);
     return number;
 }
 
@@ -62,21 +65,43 @@ function createUniqueRandomNumber(arrayOfNumber, min, max){
 
 function startGame(){
     const numbers = document.getElementsByClassName('number');
-    console.log(numbers);
     for(i=0; i < numbers.length; i++){
+        numbers[i].classList.remove('d-flex');
         numbers[i].classList.add('d-none');
     }
 }
 
-function userPrompt(){
-    let userInputNumber = [];
+function userPrompt(checkList){
+    const numbers = document.getElementsByClassName('number');
+    let userChoosenNumber = [];
+    let rightInputNumber = [];
     let number;
-    for(i=0; i < 5; i++){
+    let foundNumber = 0;
+    for(i=0; i < checkList.length; i++){
         do{
             number = parseInt(prompt(`Inserisci il ${i+1}° numero`));
         }
-        while(isNaN(number) || (number < 0 || number > 100) || userInputNumber.includes(number))
-        userInputNumber.push(number);
+        while(isNaN(number) || (number < 0 || number > 100) || userChoosenNumber.includes(number))
+        userChoosenNumber.push(number);
+        if(checkList.includes(number)){
+            foundNumber++
+            rightInputNumber.push(number);
+        } 
     }
-    return userInputNumber;
+    console.log(rightInputNumber);
+    console.log("checkList.length=",checkList.length);
+    for(i=0; i < checkList.length; i++){
+        if(userChoosenNumber.includes(checkList[i])){
+            numbers[i].classList.add('found');
+            console.log(`userChoosenNumber: ${userChoosenNumber} checkList[i]: ${checkList[i]}`);
+        } else {
+            numbers[i].classList.add('noFound');
+        }
+    }
+
+    for(i=0; i < numbers.length; i++){
+            numbers[i].classList.add('d-flex');
+        }
+        scoreDom.classList.remove('d-none');
+        scoreDom.innerHTML = `Hai indovinato ${foundNumber} numeri!`;
 }
